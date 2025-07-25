bd4f3c8b3e0955e2ec34ea6b59616ae8549c09ad
isuking6511 <isuking6511@gmail.com>
2025-07-25 01:10:00 +0000

    update application code

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host='',
+        host='lab-edu-rds-aurora.cluster-cl8e6kwi0tpw.ap-northeast-2.rds.amazonaws.com',
         database='trip_advisor',
         user='user',
         password='qwer1234'
